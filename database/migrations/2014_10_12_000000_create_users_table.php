232a0be5a9a48bad5404dba94ea66a147cf67745
fredy cumpa <fredy.acp25@gmail.com>
2022-02-28 10:01:41 -0500

    updte

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('nombre')->nullable();
             $table->string('apellido')->nullable();
             $table->string('cargo')->nullable();
