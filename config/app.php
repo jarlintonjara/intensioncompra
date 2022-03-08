009c26e6782286d7dd90c52d2ddd2aca10e53189
fredy cumpa <fredy.acp25@gmail.com>
2022-03-08 00:19:28 -0500

    migrate

--- config/app.php
+++ config/app.php
@@ -69,7 +69,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Lima',
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        'YOzaz\LaravelSwiftmailer\ServiceProvider',
 
     ],
 
@@ -191,7 +192,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // ...
+        'Mail' => 'YOzaz\LaravelSwiftmailer\Facade',
     ])->toArray(),
 
 ];
