db0343041d14e7a2d6dc97289a4ec9631650a64e
Jarlinton jara <alejarahi@gmail.com>
2022-03-18 09:43:50 -0500

    cambio de hora para probar envío de correo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:mail')->dailyAt('4:00');
+        $schedule->command('send:mail')->dailyAt('10:00');
     }
 
     /**
