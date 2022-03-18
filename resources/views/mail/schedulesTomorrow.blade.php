deb8c11dbd4d307724d9229b99e990920675c61c
Jarlinton jara <alejarahi@gmail.com>
2022-03-18 12:48:35 -0500

    test correo

--- resources/views/mail/schedulesTomorrow.blade.php
+++ resources/views/mail/schedulesTomorrow.blade.php
@@ -1,3 +1,8 @@
-<h1>
-    Buen día se envía la programación de estacionamientos.
-</h1>
+<h3>
+Hola Alessandra:<br>
+A continuación las lista de los estacionamientos asignados para el ingreso vehicular del día de mañana.<br>
+Cualquier consulta al respecto por favor comunícate con Helen al móvil 966361924.<br>
+Agradecemos las coordinaciones gestionadas.<br>
+<br>
+Atentamente,<br>
+</h3>
