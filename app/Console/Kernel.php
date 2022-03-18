906ff8fc68ceff24ff1c31b1a6a6d1bb7e5d5451
Jarlinton jara <alejarahi@gmail.com>
2022-03-18 12:34:41 -0500

    test correo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:mail')->dailyAt('13:40');	
+        $schedule->command('send:mail')->dailyAt('12:40');	
     }
 
     /**
