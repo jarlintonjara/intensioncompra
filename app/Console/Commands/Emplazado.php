dfbe4b3e8f05baa9ba7586dd371c153cfb306b6c
fredy cumpa <fredy.acp25@gmail.com>
2022-04-11 21:08:02 -0500

    cron emplazado

--- app/Console/Commands/Emplazado.php
+++ app/Console/Commands/Emplazado.php
@@ -13,8 +13,27 @@ class Emplazado extends Command
 
     public function handle()
     {
-        $registros = AsignacionModel::where('situacion', 'RESERVADO')->get();
-
+        //$registros = AsignacionModel::where('situacion', 'RESERVADO')->get();
+        $registros = AsignacionModel::select(
+            'packing_list.vin',
+            'packing_list.marca',
+            'packing_list.modelo',
+            'packing_list.version',
+            'packing_list.color',
+            'packing_list.anio_modelo',
+            'packing_list.codigo_sap',
+            'packing_list.fecha_ingreso',
+            'asignaciones.id',
+            'asignaciones.fecha_distribucion',
+            'asignaciones.codigo_reserva',
+            'asignaciones.monto_reserva',
+            'asignaciones.fecha_reserva',
+            'asignaciones.situacion'
+        )
+            ->Join('packing_list', 'asignaciones.ingreso_id', 'packing_list.id')
+            ->where('situacion', 'RESERVADO')->get();
+        
+        dd($registros);
         foreach ($registros as $registro) {
             $row = EmplazadoModel::where('vin', $registro->vin)->where('situacion', '!=', 'EMPLAZADO')->first();
             if($row){
