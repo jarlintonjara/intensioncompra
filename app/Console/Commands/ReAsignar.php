cbc2bdb5af0a6ea14529a6a21c6b831f00e0bd69
Fredy Cumpa <fredy.acp25@gmail.com>
2022-04-21 09:31:44 -0500

    re asignar

---
+++ app/Console/Commands/ReAsignar.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+use App\Models\RegistroModel;
+use App\Models\IngresoModel;
+use App\Models\AsignacionModel;
+
+use Illuminate\Console\Command;
+
+class ReAsignar extends Command
+{
+    protected $signature = 'send:reAsignar';
+
+    protected $description = 'Despues de 24h de reservarse pasan a cola de registros';
+
+    public function handle()
+    {
+        $asignaciones = AsignacionModel::where('registros.situacion', 'ASIGNADO')->get();
+
+        foreach ($asignaciones as $asignacion) {
+
+            $registro = RegistroModel::where('id', $asignacion->registro_id)
+                        ->where('estado', 1)
+                        ->first();
+    
+            if($registro){
+                $registro->situacion = 'SINASIGNAR';
+                $registro->fecha = date('Y-m-d');
+                $registro->save();
+                $asignacion->situacion = 'SINASIGNAR';
+                $asignacion->save();
+            } 
+        }
+    }
+}
