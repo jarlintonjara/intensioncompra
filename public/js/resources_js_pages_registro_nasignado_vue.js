e8eeda98bb67541e70e47f92fbb8214fe7e11a18
fredy cumpa <fredy.acp25@gmail.com>
2022-04-22 12:10:09 -0500

    titulo facturados

--- public/js/resources_js_pages_registro_nasignado_vue.js
+++ public/js/resources_js_pages_registro_nasignado_vue.js
@@ -1267,11 +1267,7 @@ var render = function () {
                         _vm._v(" "),
                         _c("td", [
                           _vm._v(
-                            _vm._s(
-                              noasignado.fecha
-                                ? _vm.$dateFormat(noasignado.fecha)
-                                : ""
-                            )
+                            _vm._s(noasignado.fecha ? noasignado.fecha : "")
                           ),
                         ]),
                       ])
